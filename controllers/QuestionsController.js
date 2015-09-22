216f4d8820df03c7b8e703f6a0be7ddfbb21b71c
Ben Spenard & Alexandra Brown <student@epicodus.com>
2015-09-22 15:47:39 -0700

    add Thank You message ngshow to submit button

--- controllers/QuestionsController.js
+++ controllers/QuestionsController.js
@@ -1,7 +1,6 @@
 surveyBot.controller('QuestionsCtrl', function QuestionsCtrl($scope, SurveyFactory) {
   $scope.answerOne = SurveyFactory.answersOne;
   $scope.answerTwo = SurveyFactory.answersTwo;
-  $scope.showEm = true;
   $scope.SurveyFactory = SurveyFactory;
 
 });
